bedd72e3866494e441f11e9215ef1f7283707863
jmorquin <j.morquin@mtp.ecolescreatives.com>
2023-05-10 16:17:08 +0200

    Ennemie

--- Assets/Ennemy_Partol.cs
+++ Assets/Ennemy_Partol.cs
@@ -6,11 +6,7 @@ public class Ennemy_Partol : MonoBehaviour
 {
     public float speed;
     public Transform[] waypoints;
-    public SpriteRenderer graphics ;
-    public int health;
-    private float dazedTime;
-    [SerializeField] private float startdazedTime;
-
+    public SpriteRenderer graphics ; 
     private Transform target;
     private int destPoint= 0 ; 
 
@@ -23,27 +19,7 @@ public class Ennemy_Partol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (dazedTime <= 0)
-
-        {
-            speed = 1;
-        }
-        else
-
-        {
-            speed = 0;
-            dazedTime -= Time.deltaTime;
-
-        }
-
-        if (health <= 0)
-
-        {
-            Destroy(gameObject);
-        }
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed* Time.deltaTime,Space.World);
+        
 
         //Si l'ennemie est quasiment arrivé à sa destination 
         if (Vector3.Distance(transform.position, target.position) < 0.3f) 
@@ -53,14 +29,5 @@ public class Ennemy_Partol : MonoBehaviour
             target = waypoints[destPoint];
             graphics.flipX = !graphics.flipX;
         }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        dazedTime = startdazedTime;
-        health -= damage;
-        Debug.Log("damage TAKEN");
-
-
     }
 }
