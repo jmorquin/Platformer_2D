1ddc0c0d664fc43bc9b20392db788963bb4d4b06
jmorquin <j.morquin@mtp.ecolescreatives.com>
2023-06-01 21:17:41 +0200

    Transition + GardeObjet

--- Assets/LoadSpecificScene.cs
+++ Assets/LoadSpecificScene.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class LoadSpecificScene : MonoBehaviour
+
 {
+    public string sceneName;
+    public Animator fadeSystem;
+    private Transform PlayerSpawn;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
+
         if (collision.CompareTag("Player"))
         {
-
-            SceneManager.LoadScene("Level_02");
+            StartCoroutine(loadNextScene());
+            fadeSystem.SetBool("FadeIn", true);
+            fadeSystem.SetBool("respawn", false);
         }
+    }
+
+    private IEnumerator loadNextScene()
+    {
+
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+        fadeSystem.SetBool("respawn", true);
+        fadeSystem.SetBool("FadeIn", false);
     }
 }
